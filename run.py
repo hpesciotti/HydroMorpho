455479d9e5b2019fc9e7e11e82989f355aa55a18
Henrique Pesciotti <hpesciotti@gmail.com>
2024-05-23 10:14:21 +0000

    chore: import RegEX for validating specific input data

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Importing Regular Expression - RegEX to validate specific patterns for morphometric input data. 
+# I used as fonts the following links:
+# How I found out about RegEX: https://stackoverflow.com/questions/18632491/how-do-i-check-for-an-exact-word-or-phrase-in-a-string-in-python
+# RegEx Calculator: https://regex101.com/
+# RegEx reading material: https://www.w3schools.com/python/python_regex.asp
+
+import re
+
+
