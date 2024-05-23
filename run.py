16a687b3be1fb525bca985a060c5c4b323306286
Henrique Pesciotti <hpesciotti@gmail.com>
2024-05-23 11:03:54 +0000

    feat: add get_data function, iterating the input parameters and retruning instructions

--- run.py
+++ run.py
@@ -6,4 +6,49 @@
 
 import re
 
+#Constants
+
+#RegEx Constant
+RE_PATTERNS = {
+    'Lat_Centroid': r'^[+-]\d{2}\.\d{6}$',
+    'Long_Centroid': r'^[+-]\d{2}\.\d{6}$',
+    'Elevation_Basin_Outlet_HBO': r'^\d{4}$',
+    'Basin_Length_Lb': r'^\d{3}\.d{3}$'
+}
+
+#Function inspired by Code Institue's Love Sandwiches project 
+def get_data(variable_input):
+    """
+    Get the basin data inputed by the user 
+    """
+    while True:
+        if variable_input == RE_PATTERNS['Lat']:
+            print("Please enter the latitude coordinate in the Decimal Degrees format of the basin's centroid.\n")
+            print("Please adhere to the specified format of the example with the exact number of digits.\n")
+            print("e.g.: -20.102852\n")
+            print('If unsure, go back to "Main Menu>Instructions" to better understand the data to be provided.\n')
+        elif variable_input == RE_PATTERNS['Long']:
+            print("Please enter the longitude coordinate in the Decimal Degrees format of the basin's centroid..\n")
+            print("Please adhere to the specified format of the example with the exact number of digits.\n")
+            print("e.g.: -43.453612\n")
+            print('If unsure, go back to "Main Menu>Instructions" to better understand the data to be provided.\n')
+        elif variable_input == RE_PATTERNS['Elevation_Basin_Outlet_HBO']:
+            print("Please enter the elevation of the basin outlet in meters.\n")
+            print("Please adhere to the specified format of the example with the exact number of digits.\n")
+            print("e.g.: 0966\n")
+            print('If unsure, go back to "Main Menu>Instructions" to better understand the data to be provided.\n')
+        elif variable_input == RE_PATTERNS['Basin_Length_Lb']:
+            print("Please enter the basin's length in kilometres.\n")
+            print("Please adhere to the specified format of the example with the exact number of digits.\n")
+            print("e.g.: 001.546\n")
+            print('If unsure, go back to "Main Menu>Instructions" to better understand the data to be provided.\n')
+        
+        data_str = input("Input the basin's data here:\n")
+
+        if validate_data(data_str, variable_input):
+            print("Data is valid")
+            break
+        return data_str
+    
+    
 
